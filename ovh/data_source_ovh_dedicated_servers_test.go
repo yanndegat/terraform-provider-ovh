76bc4812ec6034002b5dacf7a4a87763a4bcab2b
yann degat <yann.degat@corp.ovh.com>
2019-11-06 11:59:39 +0000

    new datasource: ovh_dedicated_servers

---
+++ ovh/data_source_ovh_dedicated_servers_test.go
@@ -0,0 +1,36 @@
+package ovh
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDedicatedServersDataSource_basic(t *testing.T) {
+	dedicated_server := os.Getenv("OVH_DEDICATED_SERVER_SERVICE_NAME")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckDedicatedServer(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDedicatedServersDatasourceConfig_Basic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckOutput(
+						"result",
+						dedicated_server,
+					),
+				),
+			},
+		},
+	})
+}
+
+const testAccDedicatedServersDatasourceConfig_Basic = `
+data "ovh_dedicated_servers" "servers" {}
+
+output result {
+   value = tolist(data.ovh_dedicated_servers.servers.available_services)[0]
+}
+`
